5ae9f89d83d37068fb1a67292c8ef18a415fcfda
joyfulbean <21800007@handong.edu>
2021-07-19 19:53:38 +0900

    7/19

---
+++ leetcode/Sorting/MIDDLE/sort-list.py
@@ -0,0 +1,35 @@
+# https://leetcode.com/problems/sort-list/
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        if l1 and l2:
+            if l1.val > l2.val:
+                l1, l2 = l2, l1
+                l1.next = self.mergeTwoLists(l1.next, l2)
+        return l1 or l2
+    
+    def sortList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not (head and head.next):
+            return head
+        
+        #런너 기법 활용
+        half, slow, fast = None, head, head
+        while fast and fast.next:
+            half, slow, fast = slow, slow.next, fast.next.next
+        half.next = None
+        
+        #분할 재귀 호출
+        l1 = self.sortList(head)
+        l2 = self.sortList(slow)
+        
+        return self.mergeTwoLists(l1,l2)
