744cf2706a64f4e9db688e4b1a8bcd99717407e3
joyfulbean <21800007@handong.edu>
2021-08-17 00:44:53 +0900

    8/17

--- samsung/A/pipe1.py
+++ samsung/A/pipe1.py
@@ -1,43 +1,59 @@
-# 즉, 파이프는 항상 빈 칸만 차지해야 한다.
-# 오른쪽 왼쪽 아래 대각선, 아래 이렇게 3방향, 밀면 45도씩 회전 가능
+import sys
 
-# 가장 처음에 파이프는 (1, 1)와 (1, 2)를 차지하고 있고, 방향은 가로이다. 파이프의 한쪽 끝을 (N, N)로 이동시키는 방법의 개수를 구해보자.
-
-# 갈수 있는 방향 
-# - 가로: 가로, 대각선
-# - 세로: 세로, 대각선
-# - 대각선: 가로, 세로, 대각선 
-result = 0
+input = sys.stdin.readline
 def dfs(x, y, direction):
     global result
    
-    if x > n-1 or y > n-1:
-        return
-    else:
-        if map_structure[x][y] == 1:
-            return 
-        # print(x,y)
-        if x == n-1 and y == n-1:
-            result += 1
+    if x == n-1 and y == n-1:
+        result += 1
+        return 
+    # print(x,y)
+    if direction == 0 or direction == 2:
+        if y + 1 < n:
+            if map_structure[x][y+1] == 0:
+                dfs(x, y+1, 0)
+    if direction == 1 or direction == 2:
+        if x + 1 < n:
+            if map_structure[x+1][y] == 0:
+                dfs(x+1, y, 1)
+    if x+1 < n and y+1 < n:
+        if map_structure[x+1][y] == 0 and map_structure[x][y+1] == 0 and map_structure[x+1][y+1] == 0:
+            dfs(x+1,y+1,2)
     
-    if direction == 'l':
-        for left in lefts:
-            dfs(x+left[0], y+left[1], left[2])
-    elif direction == 'd':
-        for down in downs:
-            dfs(x+down[0], y+down[1], down[2])
-    elif direction == 'c':
-        for cross in crosses:
-            dfs(x+cross[0], y+cross[1], cross[2])
-
 n = int(input())
 map_structure = [list(map(int,input().split())) for _ in range(n)]
-
-lefts = [(0,1,'l'),(1,1,'c')]
-downs = [(1,0,'d'),(1,1,'c')]
-crosses = [(0,1,'l'),(1,0,'d'),(1,1,'c')]
-
-dfs(0,1,'l')
-
+result = 0
+dfs(0,1,0)
 print(result)
 
+
+
+
+
+
+N = int(input())
+board = [list(map(int, input().split(' '))) for _ in range(N)]
+result = [[[0 for i in range(N)] for i in range(N)] for i in range(3)]
+result[0][0][1] = 1
+
+# 맨 첫줄은 가로밖에
+for i in range(2, N):
+    if board[0][i] == 0:
+        result[0][0][i] = result[0][0][i-1]
+
+# 두번째 줄부터
+for i in range(1, N):
+    # 파이프가 (0, 0) , (1, 0)에 놓여있으니까 3번째부터
+    for j in range(2, N):
+        if board[i][j] == 0 and board[i-1][j] == 0 and board[i][j-1] == 0:
+            result[2][i][j] = result[0][i-1][j-1] + result[1][i-1][j-1] + result[2][i-1][j-1]
+
+        if board[i][j] == 0:
+            # 가로
+            result[0][i][j] = result[0][i][j-1] + result[2][i][j-1]
+            # 세로
+            result[1][i][j] = result[1][i-1][j] + result[2][i-1][j]
+
+
+answer = [result[x][N-1][N-1] for x in range(3)]
+print(sum(answer))
